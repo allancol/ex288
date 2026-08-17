26f3a8bf025dfa2aed6b49cb98437fd82cd6b324
Allan Collier <allan.a.collier@igfej.mj.pt>
2026-08-17 16:53:33 +0100

    fix

--- question01/server.js
+++ question01/server.js
@@ -1,23 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const PORT = 8080;
+var express = require('express');
+app = express();
 
-let pastes = [];
-
-app.use(bodyParser.json());
-app.use(express.static('views'));
-
-app.get('/api/pastes', (req, res) => {
-    res.json(pastes);
+app.get('/', function (req, res) {
+    res.send('Hello World!\n');
 });
 
-app.post('/api/paste', (req, res) => {
-    const { text } = req.body;
-    pastes.push({ id: pastes.length + 1, text });
-    res.json({ success: true, id: pastes.length });
-});
-
-app.listen(PORT, () => {
-    console.log(`Pastebin running on port ${PORT}`);
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!');
 });
